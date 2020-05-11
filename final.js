002dac5eeb792caf7c604f33a157022fdcfd50e5
Ali <alimalik2000@gmail.com>
2020-05-11 18:08:20 -0500

    cleaned up some code

--- final.js
+++ final.js
@@ -29,7 +29,6 @@ var saturntextureNORMAL = new THREE.TextureLoader().load( 'normals/saturntexture
 var uranustextureNORMAL = new THREE.TextureLoader().load( 'normals/uranustextureNORMAL.png' );
 var neptunetextureNORMAL = new THREE.TextureLoader().load( 'normals/neptunetextureNORMAL.png' );
 
-
 //basic three setup
 var scene, renderer, camera, controls;
 
@@ -41,205 +40,247 @@ window.innerWidth/window.innerHeight, 0.1, 1000);
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-	
-
-//all planets setup
-
-//sun for sunlight
-var sun = new THREE.SphereGeometry(.001, 1, 1);
-var sunLight = new THREE.PointLight(0xffffff);
-sunLight.position.set(0, 0, 0); 
-sunLight.castShadow = true; 
-sunLight.shadowMapWidth = 1024; 
-sunLight.shadowMapHeight = 1024; 
-sunLight.shadowCameraNear = 500; 
-sunLight.shadowCameraFar = 4000;
-sunLight.add(new THREE.Mesh(sun, new THREE.MeshBasicMaterial({ color: 0x000000 })));
-sunLight.shadowCameraFov = 30;
-scene.add(sunLight);
-
-//sun with texture    
-var sun2 = new THREE.SphereGeometry(3, 50, 50);
-var material = new THREE.MeshBasicMaterial( { map: suntexture, normalMap: suntextureNORMAL} );
-var sunsphere = new THREE.Mesh( sun2, material );
-scene.add( sunsphere );
 
 
-//outer glow for sun
-var num=3.01;
-var glow = new THREE.MeshBasicMaterial( { color: 0xf00000, transparent: true, opacity: 0.03 } );
-for(var i=0;i<10;i++)
+function Main()
 {
-	var sphereGeom =  new THREE.SphereGeometry( num, 100, 100 );
-	var sphere = new THREE.Mesh( sphereGeom, glow );
-	scene.add(sphere);
-	num+=.07;
+
+	//used to cinematic zoom out
+	var x=0;
+	while(x<100)
+	{
+	zoomOut(10);
+	x++;
+	}
+
+	createLight();
+	createPlanets();
+	createBackground();
+	render();
+
 }
 
 
-//Mercury
-var geometry = new THREE.SphereGeometry(0.3, 10, 10);
-var material = new THREE.MeshPhongMaterial( { map:mercurytexture, normalMap: mercurytextureNORMAL } );
-var mercury = new THREE.Mesh( geometry, material );
-mercury.position.set(-5, 0, -5);
-scene.add(mercury);
+function createLight()
+{
 
-//Venus
-var geometry = new THREE.SphereGeometry(0.4, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:venustexture, normalMap:venustextureNORMAL } );
-var venus = new THREE.Mesh( geometry, material );
-venus.position.set(-7, 0, 7);
-scene.add(venus);
+	var lightsrc = new THREE.SphereGeometry(.001, 1, 1);
+	var sunLight = new THREE.PointLight(0xffffff);
+	sunLight.position.set(0, 0, 0); 
+	sunLight.castShadow = true; 
+	sunLight.shadowMapWidth = 1024; 
+	sunLight.shadowMapHeight = 1024; 
+	sunLight.shadowCameraNear = 500; 
+	sunLight.shadowCameraFar = 4000;
+	sunLight.add(new THREE.Mesh(lightsrc, new THREE.MeshBasicMaterial({ color: 0x000000 })));
+	sunLight.shadowCameraFov = 30;
+	scene.add(sunLight);
 
-//Earth
-var geometry = new THREE.SphereGeometry(0.6, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:earthtexture, normalMap: earthtextureNORMAL } );
-var earth = new THREE.Mesh( geometry, material );
-earth.position.set(20, 0, -20);
-scene.add(earth);
-
-//earth moon
-var geometry = new THREE.SphereGeometry(.3, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:earthmoontexture, normalMap:earthmoontextureNORMAL} );
-var earthmoon = new THREE.Mesh( geometry, material );
-earthmoon.position.set(20, .25, -20);
-scene.add(earthmoon);
-
-//Mars
-var geometry = new THREE.SphereGeometry(0.5, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:marstexture, normalMap: marstextureNORMAL } );
-var mars = new THREE.Mesh( geometry, material );
-mars.position.set(10, 0, 10);
-scene.add(mars);
-
-//Mars moon1 Phobos
-var geometry = new THREE.SphereGeometry(0.2, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:marsmoon1texture, normalMap: marsmoon1textureNORMAL } );
-var marsmoon1 = new THREE.Mesh( geometry, material );
-marsmoon1.position.set(10, 0, 10);
-scene.add(marsmoon1);
-
-//Mars moon2 Deimos
-var geometry = new THREE.SphereGeometry(0.12, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:marsmoon2texture, normalMap: marsmoon2textureNORMAL } );
-var marsmoon2 = new THREE.Mesh( geometry, material );
-marsmoon2.position.set(10, 0, 10);
-scene.add(marsmoon2);
-
-//Jupiter
-var geometry = new THREE.SphereGeometry(2, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:jupitertexture, normalMap: jupitertextureNORMAL} );
-var jupiter = new THREE.Mesh( geometry, material );
-jupiter.position.set(20, 0, -20);
-scene.add(jupiter);
-
-//Jupiter ring
-var geometry = new THREE.RingGeometry( 2.2, 3, 32 );
-var material = new THREE.MeshPhongMaterial( { map:jupitertexture, normalMap: jupitertextureNORMAL, side: THREE.DoubleSide, opacity: 0.5} );
-var jupiterring = new THREE.Mesh( geometry, material );
-jupiterring.rotation.x=11;
-jupiterring.position.set(20, 0, -20);
-scene.add( jupiterring );
-
-//Saturn
-var geometry = new THREE.SphereGeometry(1.2, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:saturntexture, normalMap: saturntextureNORMAL } );
-var saturn = new THREE.Mesh( geometry, material );
-saturn.position.set(-10, 0, -20);
-scene.add(saturn);
-
-//Saturn ring
-var geometry = new THREE.RingGeometry( 2, 2.9, 32 );
-var material = new THREE.MeshPhongMaterial( { map:saturntexture, normalMap: saturntextureNORMAL, side: THREE.DoubleSide, opacity: 0.5 } );
-var saturnring = new THREE.Mesh( geometry, material );
-saturnring.rotation.x=90;
-saturn.position.set(-10, 0, -20);
-scene.add( saturnring );
-
-//Uranus
-var geometry = new THREE.SphereGeometry(1, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:uranustexture, normalMap: uranustextureNORMAL} );
-var uranus = new THREE.Mesh( geometry, material );
-uranus.position.set(20, 0, -20);
-scene.add(uranus);
-
-//Uranus ring
-var geometry = new THREE.RingGeometry( 1.6, 2.0, 32 );
-var material = new THREE.MeshPhongMaterial( { map:uranustexture, normalMap: uranustextureNORMAL , side: THREE.DoubleSide, opacity: 0.5 } );
-var uranusring = new THREE.Mesh( geometry, material );
-uranusring.position.set(-10, 0, -20);
-uranusring.rotation.x=3;
-scene.add( uranusring );
-
-// Neptune.
-var geometry = new THREE.SphereGeometry(1, 20, 20);
-var material = new THREE.MeshPhongMaterial( { map:neptunetexture, normalMap:neptunetextureNORMAL } );
-var neptune = new THREE.Mesh( geometry, material );
-neptune.position.set(50, 0, -20);
-scene.add(neptune);
-
-//Neptune ring
-var geometry = new THREE.RingGeometry( 2.2, 2.4, 32 );
-var material = new THREE.MeshPhongMaterial( { map:neptunetexture, normalMap: neptunetextureNORMAL, side: THREE.DoubleSide , opacity: 0.5} );
-var neptunering = new THREE.Mesh( geometry, material );
-neptunering.position.set(-10, 0, -20);
-neptunering.rotation.x=-90;
-scene.add( neptunering );
-
-//Galaxy 1
-var geometry = new THREE.BoxGeometry(1, 100, 100);
-var material = new THREE.MeshBasicMaterial( { map:galaxytexture1 } );
-var galaxy1 = new THREE.Mesh( geometry, material );
-galaxy1.position.set(800, 100, 50);
-scene.add(galaxy1);
-
-//Galaxy 2
-var geometry = new THREE.BoxGeometry(100, 1, 100);
-var material = new THREE.MeshBasicMaterial( { map:galaxytexture1 } );
-var galaxy2 = new THREE.Mesh( geometry, material );
-galaxy2.position.set(200, -800, -100);
-scene.add(galaxy2);
-
-//Galaxy 3
-var geometry = new THREE.BoxGeometry(1, 100, 100);
-var material = new THREE.MeshBasicMaterial( { map:galaxytexture2 } );
-var galaxy3 = new THREE.Mesh( geometry, material );
-galaxy3.position.set(-900, 200, -400);
-galaxy3.rotation.y+=100;
-scene.add(galaxy3);
-
-//Galaxy 4
-var geometry = new THREE.BoxGeometry(100, 100, 1);
-var material = new THREE.MeshBasicMaterial( { map:galaxytexture2 } );
-var galaxy4 = new THREE.Mesh( geometry, material );
-galaxy4.position.set(100, -100, 1000);
-galaxy4.rotation.y-=50;
-scene.add(galaxy4);
-
-//stars
-var geometry2 = new THREE.Geometry();
-var totalObjects = 40000;
-var container = document.createElement('div');
-document.body.appendChild( container );
-scene.fog = new THREE.FogExp2( 0x000000, 0.001 );  
-
-for (i = 0; i < totalObjects; i ++) 
-{ 
-  var vertex = new THREE.Vector3();
-  vertex.x = (Math.random()-.5)*2000;
-  vertex.y = (Math.random()-.5)*2000;
-  vertex.z = (Math.random()-.5)*20000;
-
-  if(distanceVector(vertex, new THREE.Vector3(0,0,0))>150)
-  {
-	geometry2.vertices.push( vertex );
-  }
-  
 }
 
-var material = new THREE.ParticleBasicMaterial( { size: 3 });
-var particles = new THREE.ParticleSystem( geometry2, material );
-scene.add( particles ); 
+
+var sunsphere, sun2, mercury,venus, earth, earthmoon, mars, marsmoon1, marsmoon2, jupiter, jupiterring,
+saturn, saturnring, uranus, uranusring, neptune, neptunering;
+
+function createPlanets()
+{
+
+
+	//sun 
+	 sun2 = new THREE.SphereGeometry(3, 50, 50);
+	var material = new THREE.MeshBasicMaterial( { map: suntexture, normalMap: suntextureNORMAL} );
+	sunsphere = new THREE.Mesh( sun2, material );
+	scene.add( sunsphere );
+
+
+	//outer glow for sun
+	var num=3.01;
+	var glow = new THREE.MeshBasicMaterial( { color: 0xf00000, transparent: true, opacity: 0.03 } );
+	for(var i=0;i<10;i++)
+	{
+		var sphereGeom =  new THREE.SphereGeometry( num, 100, 100 );
+		var sphere = new THREE.Mesh( sphereGeom, glow );
+		scene.add(sphere);
+		num+=.07;
+	}
+
+
+	//Mercury
+	var geometry = new THREE.SphereGeometry(0.3, 10, 10);
+	var material = new THREE.MeshPhongMaterial( { map:mercurytexture, normalMap: mercurytextureNORMAL } );
+	mercury = new THREE.Mesh( geometry, material );
+	mercury.position.set(-5, 0, -5);
+	scene.add(mercury);
+
+	//Venus
+	var geometry = new THREE.SphereGeometry(0.4, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:venustexture, normalMap:venustextureNORMAL } );
+	venus = new THREE.Mesh( geometry, material );
+	venus.position.set(-7, 0, 7);
+	scene.add(venus);
+
+	//Earth
+	var geometry = new THREE.SphereGeometry(0.6, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:earthtexture, normalMap: earthtextureNORMAL } );
+	earth = new THREE.Mesh( geometry, material );
+	earth.position.set(20, 0, -20);
+	scene.add(earth);
+
+	//earth moon
+	var geometry = new THREE.SphereGeometry(.3, 20, 20);
+	var material = new THREE.MeshLambertMaterial( { map:earthmoontexture} );
+	earthmoon = new THREE.Mesh( geometry, material );
+	earthmoon.position.set(20, .25, -20);
+	scene.add(earthmoon);
+
+	//Mars
+	var geometry = new THREE.SphereGeometry(0.5, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:marstexture, normalMap: marstextureNORMAL } );
+	mars = new THREE.Mesh( geometry, material );
+	mars.position.set(10, 0, 10);
+	scene.add(mars);
+
+	//Mars moon1 Phobos
+	var geometry = new THREE.SphereGeometry(0.2, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:marsmoon1texture, normalMap: marsmoon1textureNORMAL } );
+	marsmoon1 = new THREE.Mesh( geometry, material );
+	marsmoon1.position.set(10, 0, 10);
+	scene.add(marsmoon1);
+
+	//Mars moon2 Deimos
+	var geometry = new THREE.SphereGeometry(0.12, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:marsmoon2texture, normalMap: marsmoon2textureNORMAL } );
+	marsmoon2 = new THREE.Mesh( geometry, material );
+	marsmoon2.position.set(10, 0, 10);
+	scene.add(marsmoon2);
+
+	//Jupiter
+	var geometry = new THREE.SphereGeometry(2, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:jupitertexture, normalMap: jupitertextureNORMAL} );
+	jupiter = new THREE.Mesh( geometry, material );
+	jupiter.position.set(20, 0, -20);
+	scene.add(jupiter);
+
+	//Jupiter ring
+	var geometry = new THREE.RingGeometry( 2.2, 3, 32 );
+	var material = new THREE.MeshPhongMaterial( { map:jupitertexture, normalMap: jupitertextureNORMAL, side: THREE.DoubleSide, opacity: 0.5} );
+	jupiterring = new THREE.Mesh( geometry, material );
+	jupiterring.rotation.x=11;
+	jupiterring.position.set(20, 0, -20);
+	scene.add( jupiterring );
+
+	//Saturn
+	var geometry = new THREE.SphereGeometry(1.2, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:saturntexture, normalMap: saturntextureNORMAL } );
+	saturn = new THREE.Mesh( geometry, material );
+	saturn.position.set(-10, 0, -20);
+	scene.add(saturn);
+
+	//Saturn ring
+	var geometry = new THREE.RingGeometry( 2, 2.9, 32 );
+	var material = new THREE.MeshPhongMaterial( { map:saturntexture, normalMap: saturntextureNORMAL, side: THREE.DoubleSide, opacity: 0.5 } );
+	saturnring = new THREE.Mesh( geometry, material );
+	saturnring.rotation.x=90;
+	saturn.position.set(-10, 0, -20);
+	scene.add( saturnring );
+
+	//Uranus
+	var geometry = new THREE.SphereGeometry(1, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:uranustexture, normalMap: uranustextureNORMAL} );
+	uranus = new THREE.Mesh( geometry, material );
+	uranus.position.set(20, 0, -20);
+	scene.add(uranus);
+
+	//Uranus ring
+	var geometry = new THREE.RingGeometry( 1.6, 2.0, 32 );
+	var material = new THREE.MeshPhongMaterial( { map:uranustexture, normalMap: uranustextureNORMAL , side: THREE.DoubleSide, opacity: 0.5 } );
+	uranusring = new THREE.Mesh( geometry, material );
+	uranusring.position.set(-10, 0, -20);
+	uranusring.rotation.x=3;
+	scene.add( uranusring );
+
+	// Neptune.
+	var geometry = new THREE.SphereGeometry(1, 20, 20);
+	var material = new THREE.MeshPhongMaterial( { map:neptunetexture, normalMap:neptunetextureNORMAL } );
+	neptune = new THREE.Mesh( geometry, material );
+	neptune.position.set(50, 0, -20);
+	scene.add(neptune);
+
+	//Neptune ring
+	var geometry = new THREE.RingGeometry( 2.2, 2.4, 32 );
+	var material = new THREE.MeshPhongMaterial( { map:neptunetexture, normalMap: neptunetextureNORMAL, side: THREE.DoubleSide , opacity: 0.5} );
+	neptunering = new THREE.Mesh( geometry, material );
+	neptunering.position.set(-10, 0, -20);
+	neptunering.rotation.x=-90;
+	scene.add( neptunering );
+
+}
+
+var galaxy1, galaxy2, galaxy3, galaxy4, particles;
+
+function createBackground()
+{
+
+		//Galaxy 1
+	var geometry = new THREE.BoxGeometry(1, 100, 100);
+	var material = new THREE.MeshBasicMaterial( { map:galaxytexture1 } );
+	galaxy1 = new THREE.Mesh( geometry, material );
+	galaxy1.position.set(800, 100, 50);
+	scene.add(galaxy1);
+
+	//Galaxy 2
+	var geometry = new THREE.BoxGeometry(100, 1, 100);
+	var material = new THREE.MeshBasicMaterial( { map:galaxytexture1 } );
+	galaxy2 = new THREE.Mesh( geometry, material );
+	galaxy2.position.set(200, -800, -100);
+	scene.add(galaxy2);
+
+	//Galaxy 3
+	var geometry = new THREE.BoxGeometry(1, 100, 100);
+	var material = new THREE.MeshBasicMaterial( { map:galaxytexture2 } );
+	galaxy3 = new THREE.Mesh( geometry, material );
+	galaxy3.position.set(-900, 200, -400);
+	galaxy3.rotation.y+=100;
+	scene.add(galaxy3);
+
+	//Galaxy 4
+	var geometry = new THREE.BoxGeometry(100, 100, 1);
+	var material = new THREE.MeshBasicMaterial( { map:galaxytexture2 } );
+	galaxy4 = new THREE.Mesh( geometry, material );
+	galaxy4.position.set(100, -100, 1000);
+	galaxy4.rotation.y-=50;
+	scene.add(galaxy4);
+
+	//stars
+	var geometry2 = new THREE.Geometry();
+	var totalObjects = 40000;
+	var container = document.createElement('div');
+	document.body.appendChild( container );
+	scene.fog = new THREE.FogExp2( 0x000000, 0.001 );  
+
+	for (i = 0; i < totalObjects; i ++) 
+	{ 
+	  var vertex = new THREE.Vector3();
+	  vertex.x = (Math.random()-.5)*2000;
+	  vertex.y = (Math.random()-.5)*2000;
+	  vertex.z = (Math.random()-.5)*20000;
+
+	  if(distanceVector(vertex, new THREE.Vector3(0,0,0))>150)
+	  {
+		geometry2.vertices.push( vertex );
+	  }
+	  
+	}
+
+	var material = new THREE.ParticleBasicMaterial( { size: 3 });
+	particles = new THREE.ParticleSystem( geometry2, material );
+	scene.add( particles ); 
+
+}
+
+
+
+
 var moveparticles=0;//used in render to move particles
 
 
@@ -367,30 +408,17 @@ function distanceVector( v1, v2 )
 }
 
 
-main();
-function main()
-{
 
 
-	//used to cinematic zoom out
-	camera.position.z=4;
-	var x=0;
-	while(x<100)
-	{
-	zoomOut(10);
-	x++;
-	}
-	
 
-	render();
-
-}
 
 var seconds = 0;
 var interval ;
 var starting=true;
 var reverse=false;
 function zoomOut(seconds) { 
+
+	camera.position.z=4;
   
   interval = setInterval(function() {
     clearInterval(interval);      
